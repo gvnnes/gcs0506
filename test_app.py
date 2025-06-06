f18b6c61fa17da68eee76b1578823f603b0e103f
Geovanne Silva de Castro <geovanne.silva@ufms.br>
2025-06-05 22:17:49 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,13 @@
+"""
+testes para app.py
+"""
+
 from app import soma
+
 def test_soma():
+"""função de teste para o arquiv app"""
   assert soma(2,3) == 5
+
+def test_main():
+  """main do testes"""
+    pass
